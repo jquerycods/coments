746ea5ac88698cf4bed8e34cb6b7084242cdc171
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-07-18 15:07:00 +0300

    Add files via upload

---
+++ coments.js
@@ -0,0 +1,6 @@
+﻿(function() {
+var items = <data:post.commentJso/>;
+var msgs = <data:post.commentMsgs/>;
+var config = <data:post.commentConfig/>;
+ //<![CDATA[
+function downloadJSAtOnload3(){var d=document.createElement("script");d.src="https://cdn.rawgit.com/jquerycods/coment/master/coment.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload3,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload3):window.onload=downloadJSAtOnload3;//]]>
